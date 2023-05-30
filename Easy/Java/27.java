586c1e4d4bd95f68a82081cf41aaf064c321bac1
Szymon <96234081+estwestminimu@users.noreply.github.com>
2023-05-30 16:27:59 +0200

    Java
    27,66

---
+++ Easy/Java/27.java
@@ -0,0 +1,17 @@
+//27. Remove Element
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int length = nums.length;
+        int k=0;
+        for(int i=0; i<length; i++)
+        {
+            if(nums[i]!=val)
+            {
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
